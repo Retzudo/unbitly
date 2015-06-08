88e9faaa907bc6e8a1ac3f5e1b224611f6c71e0b
Christoph Willinger <christoph.willinger@gmail.com>
2015-06-08 21:58:16 +0200

    Styling

--- static/js/main.js
+++ static/js/main.js
@@ -16,6 +16,7 @@
         };
 
         $scope.open = function() {
+            $scope.loading = true;
             getDecoded($scope.url).success(function(data) {
                 window.location = data.url;
             });
@@ -25,4 +26,17 @@
             return $http.post('/follow', {'url': url});
         }
     }]);
+
+    app.directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if(event.which === 13) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    });
 })(angular);
